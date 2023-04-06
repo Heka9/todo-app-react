ed2c4c5683175982df683bb2951050f999a70162
Heka9 <bliznyukjenia93@gmail.com>
2023-04-06 19:15:25 +0300

    add styles

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,13 @@
-function Todo({ todo }) {
-   return <div>{todo}</div>
+import { RiTaskLine } from 'react-icons/ri'
+import styles from './Todo.module.css'
+
+function Todo({ todo, index, deleteTodo }) {
+   return (
+      <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+         <RiTaskLine className={styles.todoIcon} />
+         <div className={styles.todoText}>{todo}</div>
+      </div>
+   )
 }
 
 export default Todo
