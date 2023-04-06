bdbba8ddd2546687a6d545b89f736bf15c8424be
Heka9 <bliznyukjenia93@gmail.com>
2023-04-06 14:41:23 +0300

    create mark up with components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from 'react'
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
 import './App.css'
 
 function App() {
-   return <div className="App"></div>
+   const [todos, setTodos] = useState(['one', 'two'])
+   return (
+      <div className="App">
+         <h1>Todo App</h1>
+         <TodoForm />
+         <TodoList todos={todos} />
+      </div>
+   )
 }
 
 export default App
---
+++ src/components/TodoForm.js
@@ -0,0 +1,10 @@
+function TodoForm() {
+   return (
+      <>
+         <input placeholder="Enter new todo" />
+         <button type="submit">Submit</button>
+      </>
+   )
+}
+
+export default TodoForm
