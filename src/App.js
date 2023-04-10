43f64632c8ae83da3c41e122aeed2bb8759f16a8
Yevhen <bliznyukjenia93@gmail.com>
2023-04-10 15:03:50 +0300

    add actiont buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import TodoList from './components/TodoList'
 import TodoForm from './components/TodoForm'
+import TodosActions from './components/TodosActions'
 import './App.css'
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
    const addTodoHandler = (text) => {
       const newTodo = {
          text,
-         isCompledet: false,
+         isCompleted: false,
          id: uuidv4(),
       }
       setTodos([...todos, newTodo])
@@ -20,11 +21,28 @@ function App() {
       setTodos(todos.filter((todo) => todo.id !== id))
    }
 
+   const toggleTodoHandler = (id) => {
+      setTodos(
+         todos.map((todo) => {
+            return todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : { ...todo }
+         })
+      )
+   }
+
+   const resetTodosHandler = () => {
+      setTodos([])
+   }
+
+   const deleteCompletedTodosHandler = () => {
+      setTodos(todos.filter((todo) => todo.isCompleted === false))
+   }
+
    return (
       <div className="App">
          <h1>Todo App</h1>
          <TodoForm addTodo={addTodoHandler} />
-         <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
+         <TodosActions resetTodos={resetTodosHandler} deleteCompletedTodos={deleteCompletedTodosHandler} />
+         <TodoList todos={todos} deleteTodo={deleteTodoHandler} toggleTodo={toggleTodoHandler} />
       </div>
    )
 }
